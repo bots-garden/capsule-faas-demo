14b55a273580a78c68c0a232dcb4e97df675031c
Philippe Charrière <ph.charriere@gmail.com>
2022-08-21 08:51:11 +0000

    📝 update hello function

--- src/functions/hello/hello.go
+++ src/functions/hello/hello.go
@@ -14,31 +14,28 @@ func Handle(bodyReq string, headersReq map[string]string) (bodyResp string, head
 	token, _ := hf.GetEnv("TOKEN")
 	html := `
     <html>
-        <head>
-            <meta charset="utf-8">
-            <title>Wasm is fantastic 😍</title>
+      <head>
+        <meta charset="utf-8">
+        <title>Wasm is fantastic 😍</title>
+				<meta name="viewport" content="width=device-width, initial-scale=1">
+				<style>
+					.container { min-height: 100vh; display: flex; justify-content: center; align-items: center; text-align: center; }
+					.title { font-family: "Source Sans Pro", "Helvetica Neue", Arial, sans-serif; display: block; font-weight: 300; font-size: 100px; color: #35495e; letter-spacing: 1px; }
+					.subtitle { font-family: "Source Sans Pro", "Helvetica Neue", Arial, sans-serif; font-weight: 300; font-size: 42px; color: #526488; word-spacing: 5px; padding-bottom: 15px; }
+					.links { padding-top: 15px; }
+				</style>
+    	</head>
 
-			<meta name="viewport" content="width=device-width, initial-scale=1">
-			
-			<style>
-				.container { min-height: 100vh; display: flex; justify-content: center; align-items: center; text-align: center; }
-				.title { font-family: "Source Sans Pro", "Helvetica Neue", Arial, sans-serif; display: block; font-weight: 300; font-size: 100px; color: #35495e; letter-spacing: 1px; }
-				.subtitle { font-family: "Source Sans Pro", "Helvetica Neue", Arial, sans-serif; font-weight: 300; font-size: 42px; color: #526488; word-spacing: 5px; padding-bottom: 15px; }
-				.links { padding-top: 15px; }
-			</style>
-
-        </head>
-
-        <body>
-			<section class="container">
-                <div>
-                    <h1 class="title">👋 Hello World 🌍</h1>
-                    <h2 class="subtitle">Served with 💜 by Capsule 💊</h2>
-                    <h2 class="subtitle">` + message + `</h2>
-                    <h2 class="subtitle">` + token + `</h2>
-                </div>
-            </section>
-        </body>
+      <body>
+				<section class="container">
+					<div>
+							<h1 class="title">👋 Hello World 🌍</h1>
+							<h2 class="subtitle">Served with 💜 by Capsule 💊</h2>
+							<h2 class="subtitle">` + message + `</h2>
+							<h2 class="subtitle">` + token + `</h2>
+					</div>
+				</section>
+      </body>
 
     </html>
     `
